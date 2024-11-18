ccf792df0e1674e0a6fa754517b31c8a3ec50e3b
Akash Tureha <akash@macbook.local>
2024-11-18 13:22:47 +0530

    added functionCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import ConnectDot from './components/ConnectDot';
+import FunctionCard from './components/FunctionCard';
 
 function App() {
   const [initialValue, setInitialValue] = useState(0);
   const [result, setResult] = useState(0);
+  const [functions, setFunctions] = useState([
+    { id: 1, expression: 'x^2', nextId: 2, position: { x: 0, y: 0 } },
+    { id: 2, expression: '2x+4', nextId: 4, position: { x: 0, y: 0 } },
+    { id: 3, expression: 'x^2+20', nextId: null, position: { x: 0, y: 0 } },
+    { id: 4, expression: 'x-2', nextId: 5, position: { x: 0, y: 0 } },
+    { id: 5, expression: 'x/2', nextId: 3, position: { x: 0, y: 0 } },
+  ]);
 
   return (
     <section className='min-w-screen min-h-screen bg-home bg-cover flex p-20 justify-between'>
@@ -25,6 +33,12 @@ function App() {
             <ConnectDot id='input-start' />
           </div>
         </div>
+      </div>
+
+      <div className='relative flex flex-wrap justify-center items-center w-[80%] gap-32'>
+        {functions.map((func) => (
+          <FunctionCard func={func} key={func.id} functions={functions} />
+        ))}
       </div>
 
       <div className='max-w-[115px] flex justify-start items-center flex-col gap-2 mt-[171px]'>
---
+++ src/components/FunctionCard.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { DragSvg } from '../assets/svg';
+import ConnectDot from './ConnectDot';
+
+interface Func {
+  id: number;
+  expression: string;
+  nextId: number | null;
+  position: { x: number; y: number };
+}
+
+interface props {
+  func: Func;
+  functions: Func[];
+}
+
+const FunctionCard = ({ func, functions }: props) => {
+  const { id, expression, nextId } = func;
+
+  const [input, setInput] = useState(expression);
+
+  return (
+    <div
+      draggable
+      className='w-[235px] h-[251px] bg-white flex flex-col justify-between items-start p-3 rounded-2xl border border-card_border shadow-[0px_0px_6px_0px_#0000000D] select-none'
+    >
+      <div className='flex w-full gap-2'>
+        <img
+          draggable={false}
+          src={DragSvg}
+          className='w-3 object-contain'
+          alt='drag'
+        />
+        <div className='text-sm text-grey_text'>Function: {id}</div>
+      </div>
+      <div className=''>
+        <label
+          htmlFor={`${id}-value`}
+          className='text-sm text-black font-medium'
+        >
+          Equation
+        </label>
+        <input
+          className='border border-input_border w-full h-[33px] outline-none px-2 rounded-lg text-sm text-black font-medium mt-1'
+          type='text'
+          name=''
+          id={`${id}-value`}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div className='w-full'>
+        <label
+          htmlFor={`${id}-nextId`}
+          className='text-sm text-black font-medium'
+        >
+          Next Function
+        </label>
+        <select
+          disabled
+          className='block w-full outline-none h-[33px] border border-input_border px-2 rounded-lg mt-1 text-sm disabled:text-grey_text font-medium'
+          value={nextId ?? ''}
+          id={`${id}-nextId`}
+        >
+          <option value={''} hidden>
+            -
+          </option>
+          {functions.map((funcOption) => {
+            return (
+              <option value={funcOption.id} key={`option-${funcOption.id}`}>
+                Function: {funcOption.id}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className='flex justify-between items-center w-full'>
+        <div className='flex gap-1 items-center'>
+          <ConnectDot id={`input-${id}`} />
+          <div>input</div>
+        </div>
+        <div className='flex gap-1 items-center'>
+          <div>output</div>
+          <ConnectDot id={`output-${id}`} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FunctionCard;
